9f99989b5d9d0f3f0fc4424402874a42c1c2313e
Brinda <brinda.srivilions9@gmail.com>
2026-08-04 08:32:22 +0530

    Add leaf emoji to app title, remove manual ABI splits (Play Store AAB handles size automatically)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -47,17 +47,7 @@ android {
             signingConfig = signingConfigs.getByName("release")
         }
     }
-
-    /*
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86_64")
-            isUniversalApk = false
-        }
-    }
-    */
+    
 }
 
 kotlin {
